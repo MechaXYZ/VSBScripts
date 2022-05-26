9ed7b3f1d632861332051dc903acd40b878ac524
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2022-05-25 21:24:37 -0500

    Update Antman.lua

--- Antman.lua
+++ Antman.lua
@@ -1,12 +1,17 @@
 -- [[ SETUP ]] --
 
+warn([[antman script by mecha
+    i recommend using r6
+    z - shrink
+    x - grow
+]])
 local currentsize = owner.Character.Head.Size.Y
 local tw = game:GetService("TweenService")
 local re = Instance.new("RemoteEvent", owner.PlayerGui)
     re.Name = "SizeEvent"
 local delay = Instance.new("NumberValue", re)
     delay.Name = "Delay"
-    delay.Value = 5
+    delay.Value = 1
 local sd = Instance.new("NumberValue", re)
     sd.Name = "Shrink"
     sd.Value = 2
@@ -23,7 +28,7 @@ local function size(scl, d)
                 C0 = CFrame.new((v.C0.Position * Percent)) * (v.C0 - v.C0.Position),
                 C1 = CFrame.new((v.C1.Position * Percent)) * (v.C1 - v.C1.Position)
             }):Play()
-		elseif v:IsA("SpecialMesh") and v.Parent.Name ~= "Head" then
+		elseif v:IsA("SpecialMesh") and (v.Parent.Name ~= "Head" or v.MeshType == Enum.MeshType.FileMesh) then
 			tw:Create(v, TweenInfo.new(d), {Scale = v.Scale * Percent}):Play()
  		elseif v:IsA("BasePart") then
             tw:Create(v, TweenInfo.new(d), {Size = v.Size * Percent}):Play() 
@@ -36,10 +41,10 @@ re.OnServerEvent:Connect(function(plr, what, type)
         if type == Enum.HumanoidRigType.R6 then
             size(currentsize / (sd.Value), delay.Value)
         elseif type == Enum.HumanoidRigType.R15 then
-            tw:Create(hum.BodyWidthScale, TweenInfo.new(delay.Value) {Value = currentsize / (sd.Value)}):Play()
-            tw:Create(hum.BodyHeightScale, TweenInfo.new(delay.Value) {Value = currentsize / (sd.Value)}):Play()
-            tw:Create(hum.BodyDepthScale, TweenInfo.new(delay.Value) {Value = currentsize / (sd.Value)}):Play()
-            tw:Create(hum.HeadScale, TweenInfo.new(delay.Value) {Value = currentsize / (sd.Value)}):Play()
+            tw:Create(hum.BodyWidthScale, TweenInfo.new(delay.Value), {Value = currentsize / (sd.Value)}):Play()
+            tw:Create(hum.BodyHeightScale, TweenInfo.new(delay.Value), {Value = currentsize / (sd.Value)}):Play()
+            tw:Create(hum.BodyDepthScale, TweenInfo.new(delay.Value), {Value = currentsize / (sd.Value)}):Play()
+            tw:Create(hum.HeadScale, TweenInfo.new(delay.Value), {Value = currentsize / (sd.Value)}):Play()
         end
         local jp = hum.JumpPower
         -- tw:Create(hum, TweenInfo.new(delay.Value), {JumpPower = jp / (sd.Value / 2)}):Play()
@@ -47,10 +52,10 @@ re.OnServerEvent:Connect(function(plr, what, type)
         if type == Enum.HumanoidRigType.R6 then
             size(currentsize * (gd.Value), delay.Value)
         elseif type == Enum.HumanoidRigType.R15 then
-            tw:Create(hum.BodyWidthScale, TweenInfo.new(delay.Value) {Value = currentsize * (gd.Value)}):Play()
-            tw:Create(hum.BodyHeightScale, TweenInfo.new(delay.Value) {Value = currentsize * (gd.Value)}):Play()
-            tw:Create(hum.BodyDepthScale, TweenInfo.new(delay.Value) {Value = currentsize * (gd.Value)}):Play()
-            tw:Create(hum.HeadScale, TweenInfo.new(delay.Value) {Value = currentsize * (gd.Value)}):Play()
+            tw:Create(hum.BodyWidthScale, TweenInfo.new(delay.Value), {Value = currentsize * (gd.Value)}):Play()
+            tw:Create(hum.BodyHeightScale, TweenInfo.new(delay.Value), {Value = currentsize * (gd.Value)}):Play()
+            tw:Create(hum.BodyDepthScale, TweenInfo.new(delay.Value), {Value = currentsize * (gd.Value)}):Play()
+            tw:Create(hum.HeadScale, TweenInfo.new(delay.Value), {Value = currentsize * (gd.Value)}):Play()
         end
         local jp = hum.JumpPower
         -- tw:Create(hum, TweenInfo.new(delay.Value), {JumpPower = jp * (gd.Value / 2.5)}):Play()
@@ -59,8 +64,6 @@ end)
 
 -- [[ LOCALSCRIPT ]] --
 NLS([[
-	print("F - Shrink")
-	print("G - Grow")
     -- SETUP --
     local re = script.Parent
     local delay = re.Delay.Value
@@ -72,38 +75,43 @@ NLS([[
     local m = owner:GetMouse()
     local db = false
     m.KeyDown:Connect(function(k)
-        if k == 'f' and db == false then
+        if k == 'z' and db == false then
             db = true
             local tween = tw:Create(cam, TweenInfo.new(delay), {FieldOfView = currentfov / sd})
-            local tween2 = tw:Create(owner.Character.Humanoid, TweenInfo.new(delay), {CameraOffset = owner.Character.Humanoid.CameraOffset + Vector3.new(0, -((currentfov / 70) / sd), 0)})
+            local tween2
+            if owner.Character.Head.Size.Y > 1 then
+                tween2 = tw:Create(owner.Character.Humanoid, TweenInfo.new(delay), {CameraOffset = owner.Character.Humanoid.CameraOffset + Vector3.new(0, -((currentfov / 70) * (sd / 2)), 0)})
+            else
+                tween2 = tw:Create(owner.Character.Humanoid, TweenInfo.new(delay), {CameraOffset = owner.Character.Humanoid.CameraOffset + Vector3.new(0, -(((currentfov / 70) / sd) * (owner.Character.Head.Size.Y * sd)), 0)})
+            end
             re:FireServer("shrink", owner.Character.Humanoid.RigType)
             tween:Play()
             tween2:Play()
             tween.Completed:Wait()
-            task.wait(.25)
+            -- if (currentfov / sd) < 1 or (currentfov / sd) > 120 then
+                currentfov = currentfov / sd
+            -- else
+                -- currentfov = cam.FieldOfView
+            -- end
+            task.wait(.05)
             db = false
-        elseif k == 'g' and db == false then
+        elseif k == 'x' and db == false then
             db = true
             local tween = tw:Create(cam, TweenInfo.new(delay), {FieldOfView = currentfov * gd})
-            local tween2 = tw:Create(owner.Character.Humanoid, TweenInfo.new(delay), {CameraOffset = owner.Character.Humanoid.CameraOffset + Vector3.new(0, ((currentfov / 70) / gd), 0)})
+            local tween2 = tw:Create(owner.Character.Humanoid, TweenInfo.new(delay), {CameraOffset = owner.Character.Humanoid.CameraOffset + Vector3.new(0, ((currentfov / 70) * gd), 0)})
             re:FireServer("grow", owner.Character.Humanoid.RigType)
             tween:Play()
             tween2:Play()
             tween.Completed:Wait()
-            task.wait(.25)
+            -- if (currentfov * gd) > 120 or (currentfov * gd) < 1 then
+                currentfov = currentfov * gd
+            -- else
+                -- currentfov = cam.FieldOfView
+            -- end
+            task.wait(.05)
             db = false
         end
     end)
-
-    -- CURRENT FOV LOOP --
-
-    coroutine.wrap(function()
-        while task.wait() do
-            pcall(function()
-                currentfov = cam.FieldOfView
-            end)
-        end
-    end)()
 ]], re)
 
 -- [[ CURRENT SIZE LOOP ]] --
@@ -114,4 +122,3 @@ coroutine.wrap(function()
         end)
     end
 end)()
-    
