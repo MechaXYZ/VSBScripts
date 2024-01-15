bbd3a50b4808afec682687096e7dd84bf431cafa
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2024-01-15 15:48:38 -0600

    Update CanvasDraw.lua

--- CanvasDraw.lua
+++ CanvasDraw.lua
@@ -595,47 +595,47 @@ do
 		FastCanvas is a modified version of Greedy/GradientCanvas by BoatBomber. 
 		This version has the ability to remove blur artefacts from the original module and improve performance. 
 		There are also some other minor additions to fit CanvasDraw's needs.
-		
+
 		Original module: https://github.com/boatbomber/GradientCanvas
-		
+
 		]]
-		
+
 		local module = {}
-		
+
 		local GuiPool = libs.GuiPool
-		--local Util = libs.Util
-		
+		--local Util = require(script.Util)
+
 		-- Micro optimisations
 		local TableInsert = table.insert
 		local TableClear = table.clear
 		local TableCreate = table.create
-		
+
 		local Mclamp = math.clamp
-		
+
 		local UDim2FromScale = UDim2.fromScale
 		local ColorSeqKeyPNew = ColorSequenceKeypoint.new
 		local ColorSeqNew = ColorSequence.new
-		
+
 		function module.new(ResX: number, ResY: number, BlurEnabled: boolean)
 			local Canvas = {
 				_Active = 0,
 				_ColumnFrames = {},
 				_UpdatedColumns = {},
-		
+
 				--Threshold = 2,
 				--LossyThreshold = 4,
 			}
-		
+
 			local invX, invY = 1 / ResX, 1 / ResY
 			--local dist = ResY * 0.03
-		
+
 			-- Generate initial grid of color data
 			local Grid = TableCreate(ResX)
 			for x = 1, ResX do
 				Grid[x] = TableCreate(ResY, Color3.new(0.98, 1, 1))
 			end
 			Canvas._Grid = Grid
-		
+
 			-- Create a pool of Frame instances with Gradients
 			do
 				local Pixel = Instance.new("Frame")
@@ -646,11 +646,11 @@ do
 				Gradient.Name = "Gradient"
 				Gradient.Rotation = 90
 				Gradient.Parent = Pixel
-		
+
 				Canvas._Pool = GuiPool.new(Pixel, ResX)
 				Pixel:Destroy()
 			end
-		
+
 			-- Create GUIs
 			local Gui = Instance.new("Frame")
 			Gui.Name = "GradientCanvas"
@@ -659,55 +659,63 @@ do
 			Gui.Size = UDim2.fromScale(1, 1)
 			Gui.Position = UDim2.fromScale(0.5, 0.5)
 			Gui.AnchorPoint = Vector2.new(0.5, 0.5)
-		
+
 			local AspectRatio = Instance.new("UIAspectRatioConstraint")
 			AspectRatio.AspectRatio = ResX / ResY
 			AspectRatio.Parent = Gui
-		
+
 			local Container = Instance.new("Folder")
 			Container.Name = "FrameContainer"
 			Container.Parent = Gui
-		
+
 			-- Define API
 			local function createGradient(colorData, x, pixelStart, pixelCount)
-				local Sequence = TableCreate(#colorData)
+				if pixelCount == 0 then
+					return
+				end
 				
+				local Sequence = TableCreate(#colorData)
 				for i, data in colorData do
+					
 					Sequence[i] = ColorSeqKeyPNew(Mclamp(data.p / pixelCount, 0, 1), data.c)
 				end
 				
 				table.sort(Sequence, function(a, b)
 					return a.Time < b.Time
 				end)
-
+				
 				local Frame = Canvas._Pool:Get()
 				Frame.Position = UDim2FromScale(invX * (x - 1), pixelStart * invY)
 				Frame.Size = UDim2FromScale(invX, invY * pixelCount)
 				Frame.Gradient.Color = ColorSeqNew(Sequence)
 				Frame.Parent = Container
-		
+
 				if Canvas._ColumnFrames[x] == nil then
 					Canvas._ColumnFrames[x] = { Frame }
 				else
 					TableInsert(Canvas._ColumnFrames[x], Frame)
 				end
-		
+
 				Canvas._Active += 1
 			end
-		
+
 			function Canvas:Destroy()
 				TableClear(Canvas._Grid)
 				TableClear(Canvas)
 				Gui:Destroy()
 			end
-		
+
 			function Canvas:SetParent(parent: Instance)
 				Gui.Parent = parent
 			end
-		
+
 			function Canvas:SetPixel(x: number, y: number, color: Color3)
 				local Col = self._Grid[x]
-		
+				
+				if not Col then
+					return
+				end
+				
 				if Col[y] ~= color then
 					Col[y] = color
 					self._UpdatedColumns[x] = Col
@@ -719,15 +727,15 @@ do
 				--if not Col then
 				--	return
 				--end
-		
+
 				return self._Grid[x][y]
 			end
-		
+
 			function Canvas:Clear(x: number?)
 				if x then
 					local column = self._ColumnFrames[x]
 					if column == nil then return end
-		
+
 					for _, object in column do
 						self._Pool:Return(object)
 						self._Active -= 1
@@ -741,33 +749,33 @@ do
 					TableClear(self._ColumnFrames)
 				end
 			end
-		
+
 			function Canvas:Render()
 				for x, column in self._UpdatedColumns do
 					self:Clear(x)
-		
+
 					local colorCount, colorData = 1, {
 						{ p = 0, c = column[1] },
 					}
-		
+
 					local pixelStart, pixelCount = 0, 0
 					--local pixelStart, lastPixel, pixelCount = 0, 0, 0
 					
 					local lastColor = column[1]
-		
+
 					-- Compress into gradients
 					for y, color in column do
 						pixelCount += 1
-		
+
 						-- Early exit to avoid the delta check on direct equality
 						if lastColor == color then
 							continue
 						end
-		
+
 						--local delta = Util.DeltaRGB(lastColor, color)
 						--if delta > self.Threshold then
 						local offset = y - pixelStart - 1
-		
+
 						--if (delta > self.LossyThreshold) or (y-lastPixel > dist) then
 						
 						TableInsert(colorData, { p = offset - 0.08, c = lastColor })
@@ -775,14 +783,14 @@ do
 						
 						TableInsert(colorData, { p = offset, c = color })
 						colorCount += 2
-		
+
 						lastColor = color
 						--lastPixel = y
-		
+
 						if colorCount > 18 then
 							TableInsert(colorData, { p = pixelCount, c = color })
 							createGradient(colorData, x, pixelStart, pixelCount)
-		
+
 							pixelStart = y - 1
 							pixelCount = 0
 							colorCount = 1
@@ -791,20 +799,20 @@ do
 						end
 						--end
 					end
-		
+
 					if pixelCount + pixelStart ~= ResY then
 						pixelCount += 1
 					end
 					TableInsert(colorData, { p = pixelCount, c = lastColor })
 					createGradient(colorData, x, pixelStart, pixelCount)
 				end
-		
+
 				TableClear(self._UpdatedColumns)
 			end
-		
+
 			return Canvas
 		end
-		
+
 		libs.FastCanvas = module
 	end
 
